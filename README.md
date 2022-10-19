dacc6c4a803c0e099ee48fbce9def08291a3b3f6
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2022-10-19 17:21:10 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# MPX
+
+Implements a pair formation model adapted from [Betti _et al_](https://doi.org/10.1101/2022.08.17.22278897) for modeling Monkeypox and Monkeypox vaccination in Mexico. 
+
+> For more information on pair formation models for sexually transmitted infections see [Kretzschmar and Heijne](https://doi.org/10.1016/j.idm.2017.07.002)
+
+## Data
+
+![Incidencia semanal de viruela símica en México](images/Monkeypox_Mx.png)
+
+Data is downloaded periodically from [Our World in Data](https://ourworldindata.org/monkeypox) which in turn results from an update from the World Health Organization. 
+
+## Model
+
+See additional information for model. 
+
+## References
+
+1. Edouard Mathieu, Fiona Spooner, Saloni Dattani, Hannah Ritchie and Max Roser (2022) - "Monkeypox". Published online at OurWorldInData.org. Retrieved from: 'https://ourworldindata.org/monkeypox' [Online Resource]
+
+2. Kretzschmar, Mirjam, and Janneke CM Heijne. "Pair formation models for sexually transmitted infections: a primer." Infectious Disease Modelling 2.3 (2017): 368-378.
+
+3. Betti, Matthew, Lauren Farrell, and Jane M. Heffernan. "A pair formation model with recovery: Application to monkeypox." medRxiv (2022).
--- get_data.R
+++ get_data.R
@@ -1,5 +1,5 @@
 rm(list = ls())
-pacman::p_load(dplyr, readr, ggplot2, cmdstanr, lubridate)
+pacman::p_load(dplyr, readr, ggplot2, lubridate, scales, ggtext)
 
 #Datos de Our World in Data
 mpx <- read_csv("https://raw.githubusercontent.com/owid/monkeypox/main/owid-monkeypox-data.csv",
@@ -26,5 +26,33 @@ mpx <- read_csv("https://raw.githubusercontent.com/owid/monkeypox/main/owid-monk
   ) |> 
   dplyr::arrange(date) |>
   readr::write_rds(file = "mpx.rds")
+
+mxplot <- ggplot(mpx) +
+  geom_col(aes(x = date, y = Casos), fill = "#d74f7c") +
+  scale_y_continuous(labels = scales::comma) +
+  labs(
+    x        = "",
+    y        = "",
+    title    = "Viruela Símica",
+    subtitle = "Nuevos casos en México (incidencia semanal)",
+    caption  = "<br>**Fuente:** Our World in Data | **Github:** @RodrigoZepeda/mpx"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title       = element_text(color = "#d74f7c", hjust = 1, size = 30, 
+                                    margin = margin(t = 10, r = 100, b = 0, l = 0, unit = "pt")),
+    plot.background  = element_rect(fill = "#45485c", color = "#45485c"),
+    panel.background = element_rect(fill = "#45485c", color = "#45485c"),
+    panel.grid       = element_blank(),
+    axis.text.y      = element_blank(),
+    axis.ticks.x     = element_line(color = "#d74f7c"),
+    axis.text.x      = element_text(color = "#d74f7c"),
+    plot.subtitle    = element_markdown(color = "#7e9bed", hjust = 1),
+    plot.caption     = element_markdown(color = "#7e9bed", hjust = 0.5)
+  ) +
+  geom_text(aes(x = date, y = Casos + 1, label = scales::comma(Casos)), vjust = 0,
+            size = 3, color = "#d74f7c")
+ggsave("images/Monkeypox_Mx.png", plot = mxplot, dpi = 750, width = 6, height = 4)
+ggsave("images/Monkeypox_Mx.pdf", plot = mxplot, dpi = 750, width = 6, height = 4)
 
 
